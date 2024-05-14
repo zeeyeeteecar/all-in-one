34dcae3655d8518c11d3db7245dd7a5b4124485c
zeeyeeteecar <tomtangjob@gmail.com>
2024-05-14 13:49:04 -0700

    1

---
+++ src/app/flexTime/components/FlexTime_Modal_StaffFlexTime_Update.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import moment from "moment";
+import { stringify } from "querystring";
+
+export default function StaffLeave_StaffLeave_Modal_SelectDate({
+  handleOnClose,
+  visible,
+}: any) {
+  if (!visible) return null;
+
+  const [selectedYearMonth, setSelectedYearMonth] = React.useState({
+    selectedYear: new Date().getFullYear(),
+    selectedMonth: new Date().getMonth() + 1,
+  });
+
+  const [
+    staffLeave_CalendarMonth,
+    setStaffLeave_CalendarMonth,
+  ] = React.useState();
+
+  const totalDaysInMonth = new Date(
+    selectedYearMonth.selectedYear,
+    selectedYearMonth.selectedMonth,
+    0
+  ).getDate();
+
+  function handle_selectLeaveDate(selectedDate: string) {
+    console.log("selectedDate:;;;", selectedDate);
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-none flex items-center justify-center">
+      <div className="w-[1000px] h-[800px] border bg-white">
+        <button onClick={handleOnClose} className="h-[30px] w-[50px]">
+          X
+        </button>
+        <div className="w-full h-[50px] border">
+          {selectedYearMonth.selectedYear.toString()} -
+          {moment(selectedYearMonth.selectedMonth.toString()).format("MMM")} ==
+          total days {totalDaysInMonth}
+        </div>
+        <div className="grid grid-cols-14">
+          {[...Array(totalDaysInMonth)].map((e, i) => {
+            const eachDate =
+              selectedYearMonth.selectedYear.toString() +
+              "-" +
+              selectedYearMonth.selectedMonth.toString() +
+              "-" +
+              (i + 1).toString();
+
+            return <div className="p-3"></div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/flexTime/components/FlexTime_StaffFlexTimeList.tsx
+++ src/app/flexTime/components/FlexTime_StaffFlexTimeList.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import moment from "moment-timezone";
-import axios from "axios";
+
+
+import FlexTime_Modal_StaffFlexTime_Update from "./FlexTime_Modal_StaffFlexTime_Update";
 
 export default function FlexTime_StaffFlexTimeList({
   staffLoginName,
@@ -14,6 +16,9 @@ export default function FlexTime_StaffFlexTimeList({
 }: any) {
   // console.log("fetch_data", fetch_data);
 
+  const [showModal_Show, setShowModal_Show] = React.useState(false);
+  const handleOnClose = () => setShowModal_Show(false);
+
   return (
     <div className="w-full h-full flex flex-col overflow-y-auto">
       <div>{staffLoginName}</div>
@@ -21,21 +26,66 @@ export default function FlexTime_StaffFlexTimeList({
         {staffFlexTime &&
           staffFlexTime.map((flextime: any, key: number) => {
             return (
-              <div key={key} className="space-x-0 flex flex-row h-10 items-center">
-                <span className="flex w-[100px] border-0">{flextime.Event_ID} </span>
+              <div
+                key={key}
+                className="space-x-2 flex flex-row h-14 items-center"
+              >
+                <span className="flex w-[100px] border-0">
+                  {flextime.Event_ID}
+                </span>
                 {/* <span>{flextime.LoginName} </span> */}
                 {/* <span>{flextime.Event_TypeCode} </span> */}
-                <span className="flex w-[150px] border-0" >{moment(flextime.Start_Date).utcOffset(0).format("YYYY-MM-DD")} </span>
+                <span className="flex w-[150px] border-0">
+                  <input
+                    type="text"
+                    className="bg-gray-50 border border-gray-200 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:outline-none focus:border-blue-500 block w-full p-2.5 "
+                    value={moment(flextime.Start_Date)
+                      .utcOffset(0)
+                      .format("YYYY-MM-DD")}
+                  />
+                </span>
                 {/* <span>{moment(flextime.End_Date).utcOffset(0).format("YYYY-MM-DD")} </span> */}
-                
-                <span className="flex w-[100px] border-0">{moment(flextime.Start_Time).format("HH:mm")} </span>
-                <span className="flex w-[100px] border-0">{moment(flextime.End_Time).format("HH:mm")} </span>
 
+                <span className="flex w-[100px] border-0">
+                  <input
+                    type="text"
+                    className="bg-gray-50 border border-gray-200 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:outline-none focus:border-blue-500 block w-full p-2.5 "
+                    value={moment(flextime.Start_Time).format("HH:mm")}
+                  />
+                </span>
+                <span className="flex w-[100px] border-0">
+                  <input
+                    type="text"
+                    className="bg-gray-50 border border-gray-200 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:outline-none focus:border-blue-500 block w-full p-2.5 "
+                    value={moment(flextime.End_Time).format("HH:mm")}
+                  />
+                </span>
+                <button
+                  id="staffID"
+                  name="staffID"
+                  type="button"
+                  className="w-[50px]  h-[50px] border hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded  items-center"
+                  value={flextime.Event_ID.toString()}
+                  // onClick={(e) => {
+                  //   handle_Select_LeaveDate(e.currentTarget.value);
+                  // }}
+                  onClick={() => setShowModal_Show(true)}
+                >
+                  {" "}
+                  cal
+                </button>
               </div>
             );
           })}
       </div>
       <div>FlexTime_StaffFlexTimeList</div>
+      <div className=" absolute  ">
+        <FlexTime_Modal_StaffFlexTime_Update
+          handleOnClose={handleOnClose}
+          visible={showModal_Show}
+
+        />
+      </div>
     </div>
   );
 }
